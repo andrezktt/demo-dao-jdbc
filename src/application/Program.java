5ee8fb2d2870597e85d7bb5d65f8310a20c22566
andre_zktt <andrezicatti@gmail.com>
2024-10-11 17:17:19 -0300

    Selller entity class

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,9 +1,16 @@
 package application;
 
 import model.entities.Department;
+import model.entities.Seller;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 
 public class Program {
     public static void main(String[] args) {
+        Department dep = new Department(1, "Books");
+        Seller sel = new Seller(1, "Vivian", "vivian@gmail.com", LocalDate.parse("15/03/2000", DateTimeFormatter.ofPattern("dd/MM/yyyy")), 2500.00, dep);
 
+        System.out.println(sel);
     }
 }
--- src/model/entities/Department.java
+++ src/model/entities/Department.java
@@ -7,6 +7,7 @@ import java.util.Objects;
 public class Department implements Serializable {
     @Serial
     private static final long serialVersionUID = 1L;
+
     private Integer id;
     private String name;
 
